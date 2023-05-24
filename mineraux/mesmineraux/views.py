a86fdb920d509a26b81867e3f4b5bc8846cdaf85
Ethan68000 <cathycoulon910@gmail.com>
2023-05-24 15:35:53 +0200

    modif

--- mineraux/mesmineraux/views.py
+++ mineraux/mesmineraux/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mesmineraux/index.html')
+
+def formulaire(request):
+    return render(request,'myfirstapp/formulaire.html')
