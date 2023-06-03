0f2ad12bc615a1ad9156a9aa98c0f4c58568285d
Ethan68000 <cathycoulon910@gmail.com>
2023-06-03 15:05:45 +0200

    modif css

--- mineraux/mesmineraux/forms.py
+++ mineraux/mesmineraux/forms.py
@@ -5,23 +5,23 @@ from . import models
 class mesminerauxForm(ModelForm):
     class Meta:
         model = models.mesmineraux
-        fields = ('nompierre', 'couleur', 'nationalité', 'poids','resume', 'livre')
+        fields = ('nompierre', 'couleur', 'nationalité', 'poids','resume',)
         labels = {
             'nompierre' : _('Nom de la pierre'),
             'couleur' : _('Quelle est sa couleur') ,
             'nationalité' : _('quelle est sa provenance'),
             'poids' : _('poids'),
             'description' : _('description'),
-            'livre' : _('livre')
         }
 
 class livreForm(ModelForm):
     class Meta:
         model = models.Livre
-        fields = ('nom_livre', 'contenu_pierre', 'page', 'resume')
+        fields = ('nom_livre', 'contenu_pierre', 'page', 'resume', 'mesmineraux')
         labels = {
             'nom_livre' : _('Nom du livre'),
             'contenu_pierre' : _('Quel pierre seront dedans'),
             'page' : _('nombre de page'),
             'resume'  : _('Resume du livre'),
+            'mesmineraux': _('mesmineraux')
         }
--- mineraux/mesmineraux/models.py
+++ mineraux/mesmineraux/models.py
@@ -6,19 +6,19 @@ class mesmineraux(models.Model):
     nationalité = models.CharField(max_length = 100)
     poids = models.IntegerField(blank=False)
     resume = models.TextField(null = True, blank = True)
-    livre = models.ForeignKey("livre", on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         chaine = f"{self.nompierre} de couleur {self.couleur} qui vient de {self.nationalité} et qui pèse {self.poids} kg"
         return chaine
     def dic(self):
-        return {"nompierre":self.nompierre, "couleur":self.couleur, "nationalité":self.nationalité, "poids":self.poids, "livre":self.livre}
+        return {"nompierre":self.nompierre, "couleur":self.couleur, "nationalité":self.nationalité, "poids":self.poids,}
 
 class Livre(models.Model):
     nom_livre = models.CharField(max_length=100)
     contenu_pierre = models.CharField(max_length=100)
     page = models.IntegerField(blank=False)
     resume = models.CharField(max_length=500)
+    mesmineraux = models.ForeignKey("mesmineraux", on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         chainelivre = f"{self.nom_livre} qui contient les pierres {self.contenu_pierre} qui fait {self.page} et qui parle de {self.resume}"
